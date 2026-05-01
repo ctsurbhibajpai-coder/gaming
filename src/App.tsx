588f644bb92e145fa5c95a9e2e94dfeb3ae57247
Your Name <you@example.com>
2026-05-01 11:07:06 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-left bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-5xl font-extralight text-blue-600 dark:text-blue-400">
+        Hello World
+      </h1>
+    </div>
+  )
+}
