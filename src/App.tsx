f9f141ae01a0928a260f5aacb8e9ec4de2f62ab5
Your Name <you@example.com>
2026-05-01 11:09:18 +0530

    textstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-left bg-gray-100 dark:bg-gray-900">
-      <h1 className="text-5xl font-extralight text-blue-600 dark:text-blue-400">
+      <h1 className="text-53xl font-extralight text-blue-600 dark:text-blue-400">
         Hello World
       </h1>
     </div>
